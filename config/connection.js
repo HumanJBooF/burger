8223ed5e7b2d2ad478fcafb7eeac493eb9dece60
humanjboof <humanjboof@gmail.com>
2018-09-23 16:26:07 -0400

    connection/jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 require('dotenv').config();
 const mysql = require('mysql');
 
-const con = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: process.env.DB_PASS,
-    database: "burgers_db"
-});
+let con;
+
+if (process.env.JAWSDB_URL) {
+    con = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    con = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.DB_PASS,
+        database: "burgers_db"
+    });
+}
 
 con.connect((err) => {
     if (err) {
